e504ae866a6231cc3a1e11889a67ea3ccd6eb72f
chunye <loseyear@gmail.com>
2019-12-02 17:25:44 +0800

    fix build.

--- cxx-top-front/config/webpack.config.js
+++ cxx-top-front/config/webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /node_modules\/(?!(dom7|swiper)\/).*/,
         use: [
           {
             loader: 'babel-loader',
